7394d65c6fbd03c01aa55b789c16fabdc7f584f4
werod45 <117578521+werod45@users.noreply.github.com>
2024-11-14 02:35:17 +0500

    yes, indeed, looks like commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+const schemeSvg = document.querySelector(".scheme_svg");
+const totalPriceTag = document.querySelector(".price_total");
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
+let cost = 800;
+let totalPrice = 0;
+schemeSvg.addEventListener("click", (event) => {
+	if(!event.target.classList.contains("booked")){
+		event.target.classList.toggle("active");
+		let totalSeats = schemeSvg.querySelectorAll(".active").length;
+		totalPrice = totalSeats * cost;
+		totalPriceTag.textContent = totalPrice;
+	}
+});
+menuButton.addEventListener("click", ()=> { 
+	menu.classList.toggle("is-open");
+});
+$(".fancybox").fancybox({
+  openEffect  : "fade",
+  closeEffect : "fade",
+  type : "iframe"
+});
